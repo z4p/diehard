7f6d5b20bfe97bf9f3bcca226b52fa33d298c709
Reuben Hawkins <rhawkins@storix.com>
2014-12-12 13:21:00 -0800

    fix compiler warnings

--- cdoperm5.c
+++ cdoperm5.c
@@ -71,21 +71,21 @@ void operm5(char *filename)
   puts("\t|rank 99).  This version uses 1,000,000 integers, twice.      |");
   puts("\t|-------------------------------------------------------------|\n");
 
-  printf("\t\t\tOPERM5 test for file \n", filename);
+  printf("\t\t\tOPERM5 test for file %s\n", filename);
   puts("\t\t  (For samples of 1,000,000 consecutive 5-tuples)\n");
 
   infile=fopen("operm5.cov", "r");
 
   for(i=0; i<60; ++i){
     for(j=i; j<60; ++j){
-      fscanf(infile, "%d", &A[i][j]);
+      fscanf(infile, "%ld", &A[i][j]);
       A[j][i]=A[i][j];
     }
   }
 
   for(i=0; i<60; ++i){
     for(j=i; j<60; ++j){
-      fscanf(infile, "%d", &B[i][j]);
+      fscanf(infile, "%ld", &B[i][j]);
       B[j][i]=B[i][j];
     }
   }
--- header.h
+++ header.h
@@ -11,7 +11,7 @@
 
 typedef unsigned int    uniform;    
 
-typedef unsigned long   counter;
+#define counter int
 
 typedef double          real;
 
